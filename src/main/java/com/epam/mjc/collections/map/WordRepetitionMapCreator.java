618652ee4b24b86d4f3ba582f3912e805d947168
Edgar Vardanyan <e.vardan4an@gmail.com>
2022-09-24 23:33:49 +0400

    first solution

--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,26 @@
 package com.epam.mjc.collections.map;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        Map<String,Integer> result = new HashMap<>();
+        String [] temp = sentence.toLowerCase().replaceAll("[\\,\\.]", "").split(" ");
+        Arrays.sort(temp);
+        Integer count = 1;
+        for (int i = 0; i < temp.length; i++) {
+            for (int j = 0; j < temp.length; j++) {
+                if (temp[j].equals(temp[i])) {
+                    count++;
+                    if (count > 1) {
+                        result.put(temp[j], count);
+                    }
+                }
+            }
+            count = 0;
+        }
+        return result;
     }
 }
